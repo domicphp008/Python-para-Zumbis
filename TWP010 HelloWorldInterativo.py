f8a7c324e5467a1da4b92f0da639dd72cbfa60cd
domicio <testedomicio@hotmail.com>
2016-01-16 05:06:56 -0200

    continuação da aula anterior

--- TWP010 HelloWorldInterativo.py
+++ TWP010 HelloWorldInterativo.py
@@ -1,6 +1,12 @@
 Python 3.5.1 (v3.5.1:37a07cee5969, Dec  5 2015, 21:12:44) 
 [GCC 4.2.1 (Apple Inc. build 5666) (dot 3)] on darwin
 Type "copyright", "credits" or "license()" for more information.
->>> print ('Alô Mundo')
-Alô Mundo
->>> 
+>>> 42
+42
+>>> 3.1415
+3.1415
+>>> type(42)
+<class 'int'>
+>>> type(3.1415)
+<class 'float'>
+>>> type('abacate')
