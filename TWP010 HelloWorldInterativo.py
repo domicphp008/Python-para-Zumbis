9c7d0902495b689e1c23029b58c96f7caa1ec5ef
domicio <testedomicio@hotmail.com>
2016-01-16 04:58:51 -0200

    TWP010

---
+++ TWP010 HelloWorldInterativo.py
@@ -0,0 +1,6 @@
+Python 3.5.1 (v3.5.1:37a07cee5969, Dec  5 2015, 21:12:44) 
+[GCC 4.2.1 (Apple Inc. build 5666) (dot 3)] on darwin
+Type "copyright", "credits" or "license()" for more information.
+>>> print ('Alô Mundo')
+Alô Mundo
+>>> 
